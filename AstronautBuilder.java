6cdc90da61cf2f7dae3b1fd52612a68bc5c61eb4
Justin Kruskie <justin@kruskie.net>
2024-09-24 19:07:59 -0400

    Create AstronautBuilder.java

---
+++ AstronautBuilder.java
@@ -0,0 +1,20 @@
+public class AstronautBuilder {
+    // Create your fields for name, age, address, and phone number.
+
+    // The setName method sets the person's name.
+
+    // The setAge method sets the person's age.
+
+    // The setAddress method sets the person's address.
+
+    // The setPhone method sets the person's phone number.
+
+    // The getName method returns the person's name.
+
+    // The getAge method returns the person's age.
+
+    // The getAddress method returns the person's address.
+
+    // The getPhone method returns the person's phone number.
+
+}
